9ae54d521d81915c1e10da2d5e23e55c1ea472ef
Remmo <remmo@Remmos-MBP.home>
2020-08-26 21:08:06 +0200

    small fix

--- src/layout/Checkout/Chekout.js
+++ src/layout/Checkout/Chekout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core";
 import CurrencyFormat from "react-currency-format";
 import CustomButton from "../../components/CustomButton/CustomButton";
